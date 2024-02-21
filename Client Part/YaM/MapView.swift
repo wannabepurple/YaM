52dfc1bff92e465488e592ea51f2b881d9959ad2
prophet satin <prophetsatin@gmail.com>
2024-02-21 22:42:53 +0300

    yam.wannabepurple

--- Client Part/YaM/MapView.swift
+++ Client Part/YaM/MapView.swift
@@ -4,6 +4,7 @@ import SwiftUI
 struct MapView: View {
     @StateObject private var viewModel = MapViewModel()
     @State private var position: MapCameraPosition = .userLocation(fallback: .automatic)
+    private let locationManager = CLLocationManager()
     
     var body: some View {
         Map(position: $position) {
@@ -14,7 +15,25 @@ struct MapView: View {
             MapPitchToggle()
         }
         .accentColor(.purple)
+        .onAppear {
+            setupLocationManager()
+            startLocationUpdateTimer()
+        }
+        
     }
+    
+    private func setupLocationManager() {
+        locationManager.delegate = viewModel
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        print(locationManager.location!.coordinate)
+    }
+    
+    private func startLocationUpdateTimer() {
+            Timer.scheduledTimer(withTimeInterval: 5.0, repeats: true) { timer in
+                setupLocationManager()
+            }
+        }
 }
 
 
